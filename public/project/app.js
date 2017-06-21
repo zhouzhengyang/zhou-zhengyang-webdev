a00bd47f0cd7be9024e558b670e270ae50586e38
zhouzhen <zhouzhen@ccs.neu.edu>
2017-06-21 19:47:56 -0400

    update project

--- public/project/app.js
+++ public/project/app.js
@@ -10,3 +10,12 @@
 // const AniListAPI = require('anilist-api-pt');
 //
 // const anilistApi = new AniListAPI({dogleftko-4qtmy, sNLBKnmKZZhxfFPhDiy7zum5xF});
+
+
+// var client_id = 'dogleftko-4qtmy';
+// var client_secret = 'sNLBKnmKZZhxfFPhDiy7zum5xF';
+// const AniListAPI = require('anilist-api-pt');
+//
+// const anilistApi = new AniListAPI({client_id, client_secret});
+//
+// anilistApi.manga.getGenres();
