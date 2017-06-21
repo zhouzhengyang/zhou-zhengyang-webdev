493f5bd838e86e765d2e3282eb9dcd56f845e98c
zhouzhen <zhouzhen@ccs.neu.edu>
2017-06-20 20:10:59 -0400

    heroku?????

--- server.js
+++ server.js
@@ -22,10 +22,9 @@ app.use(passport.session());
 // configure a public directory to host static content
 app.use(app.express.static(__dirname + '/public'));
 
-require ("./test/app.js");
+require ("./test/app.js")(app);
 require ("./assignment/app.js");
 
 //var port = process.env.PORT || 3000;
 
 app.listen(process.env.PORT || 3000);
-
