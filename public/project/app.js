417c40fe5e38ef15cb3efc0d389bfe1085600c2d
zhouzhen <zhouzhen@ccs.neu.edu>
2017-06-20 21:42:20 -0400

    small change

---
+++ public/project/app.js
@@ -0,0 +1,12 @@
+(function () {
+    angular
+        .module("ProjectApp", [])
+        .controller("ApiController", ApiController);
+})();
+
+
+
+
+// const AniListAPI = require('anilist-api-pt');
+//
+// const anilistApi = new AniListAPI({dogleftko-4qtmy, sNLBKnmKZZhxfFPhDiy7zum5xF});
