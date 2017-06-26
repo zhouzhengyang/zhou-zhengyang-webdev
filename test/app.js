efb97997944a1d5c1c0db89e2689f2104f9fd45c
zhouzhen <zhouzhen@ccs.neu.edu>
2017-06-25 20:52:37 -0400

    homepage

--- test/app.js
+++ test/app.js
@@ -13,7 +13,7 @@ module.exports = function(app)
     }
 
     var mongoose = require("mongoose");
-    mongoose.connect(connectionString);
+    mongoose.connect(connectionString, {useMongoClient: true});
 
     var TestSchema = mongoose.Schema({
         message: String
